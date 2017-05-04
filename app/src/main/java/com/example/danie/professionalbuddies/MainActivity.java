7ab70bd406084a10e26b6d4bd1fb069b1216a9d5
nugready <danielnugready@gmail.com>
2017-05-03 23:39:39 -0700

    Slightly altered layout; buttons clickable to transition to new activities (which are still empty).

--- app/src/main/java/com/example/danie/professionalbuddies/MainActivity.java
+++ app/src/main/java/com/example/danie/professionalbuddies/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.danie.professionalbuddies;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,18 +14,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void googleLogin(View view) {
+        Intent intent = new Intent(this, googleLoginActivity.class);
+        startActivity(intent);
         // Transfer to google login activity
     }
 
     public void login(View view) {
+        // Need to evaluate whether or not
+        // valid username & password have
+        // been entered before switching
+        // to next activity
+
         // Transfer to login activity
+        Intent intent = new Intent(this, loginActivity.class);
+        startActivity(intent);
     }
 
     public void accountRecovery(View view) {
+        Intent intent = new Intent(this, accountRecoveryActivity.class);
+        startActivity(intent);
         // Transfer to account recovery acitivity
     }
 
     public void newUser(View view) {
         // Transfer to new user sign-up activity
+        Intent intent = new Intent(this, RegistrationActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/danie/professionalbuddies/accountRecoveryActivity.java
@@ -0,0 +1,13 @@
+package com.example.danie.professionalbuddies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class accountRecoveryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_recovery);
+    }
+}
