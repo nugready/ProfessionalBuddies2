a916e37f335e640303e247b94ae2ff6b63f3be43
janiceambrose <janiceambrose1@gmail.com>
2017-05-11 13:03:54 -0700

    Questions

--- app/src/main/java/com/example/danie/professionalbuddies/RegistrationActivity.java
+++ app/src/main/java/com/example/danie/professionalbuddies/RegistrationActivity.java
@@ -2,12 +2,17 @@ package com.example.danie.professionalbuddies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class RegistrationActivity extends AppCompatActivity {
+    Spinner spinnerQ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        spinnerQ = (Spinner) findViewById(R.id.spinner);
+
+        spinnerQ.setOnItemSelectedListener();
     }
 }
