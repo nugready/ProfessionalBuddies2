4814e1833cdf9382ad961686e93f5b7cd9bb775a
janiceambrose <janiceambrose1@gmail.com>
2017-05-11 16:22:06 -0700

    Questions

--- app/src/main/java/com/example/danie/professionalbuddies/RegistrationActivity.java
+++ app/src/main/java/com/example/danie/professionalbuddies/RegistrationActivity.java
@@ -2,6 +2,8 @@ package com.example.danie.professionalbuddies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Spinner;
 
 public class RegistrationActivity extends AppCompatActivity {
@@ -11,8 +13,26 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
         spinnerQ = (Spinner) findViewById(R.id.spinner);
 
-        spinnerQ.setOnItemSelectedListener(new);
+        spinnerQ.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+              public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                parent.getItemAtPosition(position);
+
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //not required now
+            }
+        });
+
+
+
+
     }
 }
