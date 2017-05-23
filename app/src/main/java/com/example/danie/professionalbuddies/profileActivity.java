dc4a6dd04aba9278ba76075ed210b26c96688f59
janiceambrose <janiceambrose1@gmail.com>
2017-05-23 13:33:16 -0700

    Profile page

---
+++ app/src/main/java/com/example/danie/professionalbuddies/profileActivity.java
@@ -0,0 +1,20 @@
+package com.example.danie.professionalbuddies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * Created by janiceambrose on 5/23/17.
+ */
+
+public class  profileActivity extends AppCompatActivity {
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+
+
+}
